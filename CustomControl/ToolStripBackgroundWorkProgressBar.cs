b92c1610caf82fe6fc6c356cfeb0740351bfe464
LAVN <rairairakurakulilac@hotmail.co.jp>
2011-10-07 21:22:07 +0900

    Project CustomControl ToolStripSpringProgressBar 追加

--- CustomControl/ToolStripBackgroundWorkProgressBar.cs
+++ CustomControl/ToolStripBackgroundWorkProgressBar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
@@ -48,6 +49,12 @@ namespace CustomControl
             set { backgroundWorkerMainProcess.WorkerSupportsCancellation = value; }
         }
         #endregion
+        [Browsable(true)]
+        public bool Spring
+        {
+            get;
+            set;
+        }
         #region ProgressBarのメソッド
         /// <summary>
         /// System.ComponentModel.BackgroundWorker.RunWorkerAsync()が呼び出されたときに発生します
@@ -166,6 +173,73 @@ namespace CustomControl
             // Cleanup
         }
 
+        public override Size GetPreferredSize(Size constrainingSize)
+        {
+            if (Spring)
+            {
+                // Use the default size if the text box is on the overflow menu
+                // or is on a vertical ToolStrip.
+                if (IsOnOverflow || Owner.Orientation == Orientation.Vertical)
+                {
+                    return DefaultSize;
+                }
+
+                // Declare a variable to store the total available width as 
+                // it is calculated, starting with the display width of the 
+                // owning ToolStrip.
+                Int32 width = Owner.DisplayRectangle.Width;
+
+                // Subtract the width of the overflow button if it is displayed. 
+                if (Owner.OverflowButton.Visible)
+                {
+                    width = width - Owner.OverflowButton.Width -
+                        Owner.OverflowButton.Margin.Horizontal;
+                }
+
+                // Declare a variable to maintain a count of ToolStripSpringProgressBar 
+                // items currently displayed in the owning ToolStrip. 
+                Int32 springBoxCount = 0;
+
+                foreach (ToolStripItem item in Owner.Items)
+                {
+                    // Ignore items on the overflow menu.
+                    if (item.IsOnOverflow) continue;
+
+                    if (item is ToolStripBackgroundWorkProgressBar)
+                    {
+                        // For ToolStripSpringProgressBar items, increment the count and 
+                        // subtract the margin width from the total available width.
+                        springBoxCount++;
+                        width -= item.Margin.Horizontal;
+                    }
+                    else
+                    {
+                        // For all other items, subtract the full width from the total
+                        // available width.
+                        width = width - item.Width - item.Margin.Horizontal;
+                    }
+                }
+
+                // If there are multiple ToolStripSpringProgressBar items in the owning
+                // ToolStrip, divide the total available width between them. 
+                if (springBoxCount > 1) width /= springBoxCount;
+
+                // If the available width is less than the default width, use the
+                // default width, forcing one or more items onto the overflow menu.
+                if (width < DefaultSize.Width) width = DefaultSize.Width;
+
+                // Retrieve the preferred size from the base class, but change the
+                // width to the calculated width. 
+                Size size = base.GetPreferredSize(constrainingSize);
+                size.Width = width;
+                return size;
+            }
+            else
+            {
+                return this.Size;
+            }
+        }
+        
         #region backgroundWorkerProgressBar
         public DoWorkEventHandler e;
         private void backgroundWorkerProgressBar_DoWork(object sender, DoWorkEventArgs e)
